d3cda585874d27348a155d8b06440c5d60259da1
inq8r <playarsenij2@gmail.com>
2025-09-20 18:48:00 +0300

    Module improvement database.py
    *Added the return of all values except the service "_id" and "_rev"

--- database.py
+++ database.py
@@ -7,9 +7,11 @@ class DataBaseAPI:
     def __init__(self, server_addr: str = 'http://127.0.0.1:5984', database_name: str = 'your_db'):
         self.database = couchdb.Server(server_addr)[database_name]
 
-    def get_keys(self, id_document: str = 'your_id') -> list[str]:
+    def get_keys(self, id_document: str = 'your_id') -> dict.values:
         response = self.database.get(id_document)
-        return response['your_index']
+        del response['_id']
+        del response['_rev']
+        return response.values()
 
 
 def __tests():
