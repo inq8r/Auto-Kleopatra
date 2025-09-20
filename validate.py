ee1762960d782edc74a96b749991091fc60eeef6
inq8r <playarsenij2@gmail.com>
2025-09-20 20:52:49 +0300

    Update validate.py
    Adding documentation to the code

--- validate.py
+++ validate.py
@@ -7,6 +7,10 @@ warnings.filterwarnings(action='ignore')
 
 
 class VerificationInterface:
+    """
+    An interface for the possibility of creating successor classes that carry different verification logic
+    """
+    
     data_to_validate: verification_data.VerificationData
 
     def _reading_file(self) -> str:
@@ -33,18 +37,23 @@ class VerificationDisconnectedData(VerificationInterface):
         sig = pgpy.PGPSignature.from_file(filename=self.data_to_validate.signature_file)
 
         for k in keys:
+            # The syntax required by the pgpy module
             pub, _ = pgpy.PGPKey.from_blob(blob=k)
             try:
                 if pub.verify(subject=file, signature=sig):
                     print(f'Подпись для файла {self.data_to_validate.file} действительна.')
                     return
+            # Skip an incorrect signature error
             except pgpy.errors.PGPError:
                 continue
         print(f'Подпись для файла {self.data_to_validate.file} недействительна.')
 
 
 class ValidateService:
-
+    """
+    A class that encapsulates the logic and results of various objects
+    """
+    
     @staticmethod
     def do_validate(checked_data_obj: verification_data.VerificationData):
         verify = VerificationDisconnectedData(data_to_validate=checked_data_obj)
